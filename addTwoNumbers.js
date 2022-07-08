25372e9b321e205f945d9293bf3f53d4cc7d2d93
kiranlakhani20 <55124757+kiranlakhani20@users.noreply.github.com>
2022-07-08 13:34:31 +0530

    Create addTwoNumbers.js

---
+++ addTwoNumbers.js
@@ -0,0 +1,10 @@
+const num1 = 5;
+const num2 = 3;
+const num3;
+num4; // add bug
+
+const sum = num1 + num2;
+
+sumRes = sum; // bad code
+
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sumRes);
